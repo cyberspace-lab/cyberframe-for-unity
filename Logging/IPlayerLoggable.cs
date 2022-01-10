839489ce8365f6e46caeec78ecfcf87ea35bd4d6
Lukáš Hejtmánek <hejtmy@gmail.com>
2022-01-10 02:43:44 +0100

    player loggable cleanup

--- Logging/IPlayerLoggable.cs
+++ Logging/IPlayerLoggable.cs
@@ -1,18 +1,12 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class IPlayerLoggable : MonoBehaviour
+namespace cyberframe.Logging
 {
-    // Start is called before the first frame update
-    void Start()
+    public interface IPlayerLoggable
     {
-        
+        string HeaderLine();
+        List<string>  PlayerInformation();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
